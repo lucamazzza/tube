dfeaddef4bb072ef55716b0651a3c6e3dc53c2d6
Luca Mazza <luca.mazza@mazluc.ch>
2024-11-24 13:18:11 +0100

    boh

--- src/tube.c
+++ src/tube.c
@@ -1,84 +1,109 @@
 // TODO: Comment Functions and Structs
 
+// START IMPORTS
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+// END IMPORTS
 
+// START DEFINES
 #define MAX_N 10000
+// END  DEFINES
 
+// START STRUCTS
 typedef struct {
   int time;
   int parent;
   int id;
 } Joint;
+// END STRUCTS
 
+// START GLOBALS
 Joint joints[MAX_N];
 int children[MAX_N][MAX_N];
-int child_count[MAX_N];
-int nullifyments[MAX_N][2];
-int leaf[MAX_N];
-int mem[MAX_N];
+int childc[MAX_N];
+int leaves[MAX_N];
+int memory[MAX_N];
+// END GLOBALS
 
-int compare(const void *a, const void *b) { return (*(int *)a - *(int *)b); }
+// START DEFINITIONS
+int dfs(int node, int *dl);
+int minimize_time(int root, int N, int C, int k);
+// END DEFINITIONS
 
 // TODO: Document as flowchart
 int dfs(int node, int *dl) {
-  if (mem[node] != -1) {  
-    *dl = leaf[node];
-    return mem[node];
+  if (memory[node] != -1) {
+    *dl = leaves[node];
+    return memory[node];
   }
-  int max_branch_weight = 0;
-  int max_leaf = node;
-  for (int i = 0; i < child_count[node]; i++) {
-    int child = children[node][i];
+  int mb = 0;
+  int ml = node;
+
+  for (int i = 0; i < childc[node]; i++) {
     int child_leaf;
+    int child = children[node][i];
     int child_weight = dfs(child, &child_leaf);
-    if (child_weight > max_branch_weight) {
-      max_branch_weight = child_weight;
-      max_leaf = child_leaf;
+    if (child_weight > mb) {
+      mb = child_weight;
+      ml = child_leaf;
     }
   }
-  mem[node] = joints[node].time + max_branch_weight;
-  leaf[node] = max_leaf;
-  *dl = max_leaf;
-  return mem[node];
+  memory[node] = joints[node].time + mb;
+  leaves[node] = ml;
+  *dl = ml;
+  return memory[node];
 }
 
 int minimize_time(int root, int N, int C, int k) {
   if (N <= C)
     return 0;
-  if (C == 0) {
-    for (int i = 0; i < 30; i++) {
-        //printf("%d | %d\n", nullifyments[i][0], nullifyments[i][1]);
-    }
-    return nullifyments[0][1]; 
-  }
-  int leaf, hb = dfs(root, &leaf);
+  if (C == 0)
+    return dfs(root, &k);
+  int leaf = -1;
+  int hb = dfs(root, &leaf);
+  int bt = hb;
+  int bj = leaf;
+
+  printf("\nC = %d------------------\n", C);
+  printf("HB: %d\n", hb);
+  printf("Start: %d\n", leaf);
+
   while (leaf != -1) {
-    int dummyptr = 0, joint_time = joints[leaf].time, mem_leaf = mem[leaf];
-    joints[leaf].time = 0;
-    mem[leaf] = -1;
-    hb = dfs(root, &dummyptr);
-    nullifyments[k][0] = leaf;
-    nullifyments[k][1] = hb;
-    joints[leaf].time = joint_time;
-    mem[leaf] = mem_leaf;
-    int parent = joints[leaf].parent;
-    leaf = parent;
+    if (joints[leaf].time != 0) {
+      int joint_time = joints[leaf].time;
+      int mem_time = memory[leaf];
+      int dummy = 0;
+      joints[leaf].time = 0;
+      hb = dfs(root, &dummy);
+      joints[leaf].time = joint_time;
+      memory[leaf] = mem_time;
+      if (hb < bt) {
+        bt = hb;
+        bj = leaf;
+
+        printf("New HB: %d\n", hb);
+      }
+    }
+    leaf = joints[leaf].parent;
     k++;
   }
-  qsort(nullifyments, k, sizeof(nullifyments[0]), compare);
-  joints[nullifyments[0][0]].time = 0;
-  return minimize_time(root, N, C - 1, 0);
+
+  printf("Removed %d, with weight %d\n", bj, joints[bj].time);
+
+  joints[bj].time = 0;
+
+  printf("K: %d\n", k);
+
+  return minimize_time(root, N, C - 1, k);
 }
 
 int main() {
   int N, C;
   scanf("%d %d", &N, &C);
   for (int i = 0; i < N; i++) {
-    mem[i] = -1;
-    child_count[i] = 0;
-    nullifyments[i][0] = -1;
-    nullifyments[i][1] = -2;
+    memory[i] = -1;
+    childc[i] = 0;
   }
   for (int i = 0; i < N; i++) {
     int t, p;
@@ -86,10 +111,8 @@ int main() {
     joints[i].time = t;
     joints[i].parent = p;
     joints[i].id = i;
-    if (p != -1) {
-      children[p][child_count[p]++] = i;
-    }
-    //printf("Joint %d, (parent: %d)\n", joints[i].id, joints[i].parent);
+    if (p != -1)
+      children[p][childc[p]++] = i;
   }
   int root = -1;
   for (int i = 0; i < N; i++) {
