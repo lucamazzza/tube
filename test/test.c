9472a83535f0ceaa538cff534be132696eaac1bd
Luca Mazza <luca.mazza@student.supsi.ch>
2025-01-08 09:17:29 +0100

    test

--- test/test.c
+++ test/test.c
@@ -28,7 +28,7 @@ int test_task(const char *name) {
     char cmd[100], fname[100], fres[100];
     strcpy(cmd, "{ ./src/tube < test/io/");
     strcat(cmd, name);
-    strcat(cmd, ".txt > tmp.txt; } 2>> errors.txt");
+    strcat(cmd, ".txt > tmp.txt; } 2> errors.txt");
     strcpy(fname, "test/io/");
     strcat(fname, name);
     strcpy(fres, fname);
@@ -37,7 +37,7 @@ int test_task(const char *name) {
     clock_t start = clock();
     system(cmd);
     clock_t end = clock();
-    float time_taken = (float)(end - start) / CLOCKS_PER_SEC;
+    float time_taken = ((float)(end - start) / CLOCKS_PER_SEC) * 1000;
     times[current_task++] = time_taken;
     FILE *file  = fopen("tmp.txt", "r");
     FILE *efile = fopen("errors.txt", "r");
@@ -48,7 +48,7 @@ int test_task(const char *name) {
     fscanf(rfile, "%d", &res);
     fscanf(efile, "%s", &errors);
     printf("%-20s", name);
-    printf(ANSI_COLOR_GREY "%-20f" ANSI_COLOR_RESET, time_taken);
+    printf(ANSI_COLOR_GREY "%-12d %-12d %-20f" ANSI_COLOR_RESET, res, out,  time_taken);
     if (strcmp(&errors, "")) {
         return -1;
     }
@@ -59,27 +59,27 @@ int main() {
     int passed  = 0;
     int failed  = 0;
     int total   = 0;
-    printf(ANSI_INVERT_BOLD "Task                Time taken [s]      Status" ANSI_REGULAR "\n");
+    printf(ANSI_INVERT_BOLD "Task                Expected     Got         Time taken [ms]      Status" ANSI_REGULAR "\n");
     for(int i = 10; i <= 10000;) {
         for(int j = 1; j <= 100;) {
-           char tname[100];
-           sprintf(tname, "task_%d_%d", i, j);
-           int test = test_task(tname);
-           if (test == 1) {
-               printf(ANSI_COLOR_GREEN "Passed" ANSI_COLOR_RESET "\n");
-               passed++;
-           } else if (test == -1) {
-               printf(ANSI_COLOR_BRED "Errors" ANSI_COLOR_RESET "\n");
-               failed++;
-           } else {
-               printf(ANSI_COLOR_RED "Failed" ANSI_COLOR_RESET "\n");
-               failed++;
-           }
-           total++;
-           if (j == 1)                  j = 10;
-           else if (j == 10 || j == 50) j *= 2;
-           else if(j == 20)             j = 50;
-           else break;
+            char tname[100];
+            sprintf(tname, "task_%d_%d", i, j);
+            int test = test_task(tname);
+            if (test == 1) {
+                printf(ANSI_COLOR_GREEN "Passed" ANSI_COLOR_RESET "\n");
+                passed++;
+            } else if (test == -1) {
+                printf(ANSI_COLOR_BRED "Errors" ANSI_COLOR_RESET "\n");
+                failed++;
+            } else {
+                printf(ANSI_COLOR_RED "Failed" ANSI_COLOR_RESET "\n");
+                failed++;
+            }
+            total++;
+            if (j == 1)                  j = 10;
+            else if (j == 10 || j == 50) j *= 2;
+            else if(j == 20)             j = 50;
+            else break;
         }
         if (i == 10)        i = 100;
         else if (i == 100)  i = 300;
@@ -97,4 +97,3 @@ int main() {
     printf("\nAverage time taken: %fs\n", avg_time());
     return 0;
 }
-
